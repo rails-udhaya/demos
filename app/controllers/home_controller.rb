f8072e330a7d3ff34b23566040b08c9929ff75cb
udhaya <udhaya.dhanabalan@gmail.com>
2014-06-24 10:22:51 -0700

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+		
+		def index
+				
+		end
+		
+		def d3_tree_chart
+		end
+		
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demos::Application.config.secret_token = '48c6793f4089fa437f17e788236366e0d80df64ee5789e1929094d5d67ec97a5d0e7be16e357b20c001161e6023c9d63a22a2af1616c93c97d1e2075713b8404'
