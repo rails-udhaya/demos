cdfe54b462cf4cd20270cd218ba583c90f7dbf71
udhaya <udhaya.dhanabalan@gmail.com>
2014-06-29 15:33:04 -0700

    change sin email

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -36,7 +36,7 @@ class HomeController < ApplicationController
 @id=params[:stubhub_url].split("-").last.gsub("/","")
 			doc = Nokogiri::HTML(open("http://www.stubhub.com/ticketAPI/restSvc/event/#{@id}"))
 		@dataTable = doc.css('p')
-		Emailer.check_email("#{Rails.env} #{params[:stubhub_url]} stubhub_data && fetch_stubhub_data ","Testing MyDemo").deliver
+		Emailer.check_email("#{params[:stubhub_url]}"," Testing #{Rails.env} fetch_stubhub_data").deliver
 		#~ @dataTable = JSON.parse(dataTable.children.to_xml)
 		#~ @dataTable = doc.css('div.dataTables_wrapper')
 
