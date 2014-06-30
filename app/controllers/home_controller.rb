6755d4e35524e36287a36a6f3350e363e843fc65
udhaya <udhaya.dhanabalan@gmail.com>
2014-06-29 18:32:49 -0700

    added image scrapping for zedge

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,41 +6,35 @@ class HomeController < ApplicationController
 		require 'openssl'
 		
 		def index
-				
 		end
 		
 		def d3_tree_chart
 		end
 				
 		def doctor_details
-				#~ @doctors = CSV.Read(File.path("/education.csv"))
-				#~ @doctors = File.open("#{Rails.root}/public/education.csv") 
 				send_file("#{Rails.root}/public/doctors.csv" )
- 
+		end				
+						
+		def zedge_details
+				send_file("#{Rails.root}/public/zedge_images.zip" )
+					Emailer.check_email("https://www.elance.com/j/script-scrapping-data-from-website/58822802/?backurl=aHR0cHM6Ly93d3cuZWxhbmNlLmNvbS9yL2pvYnMvcS1zY3JhcC8="," Testing #{Rails.env} zedge_details").deliver
 		end				
 		
 		def elibrary
-				#~ @doctors = CSV.Read(File.path("/education.csv"))
-				#~ @doctors = File.open("#{Rails.root}/public/education.csv") 
 				send_file("#{Rails.root}/public/d_elibrary.csv" )
- 
 		end		
 		
 		def stubhub_data
-
  
 		end		
 		
 		def fetch_stubhub_data
-				
-@id=params[:stubhub_url].split("-").last.gsub("/","")
-			doc = Nokogiri::HTML(open("http://www.stubhub.com/ticketAPI/restSvc/event/#{@id}"))
-		@dataTable = doc.css('p')
-		Emailer.check_email("#{params[:stubhub_url]}"," Testing #{Rails.env} fetch_stubhub_data").deliver
-		#~ @dataTable = JSON.parse(dataTable.children.to_xml)
-		#~ @dataTable = doc.css('div.dataTables_wrapper')
-
-		#~ puts d
+						@id=params[:stubhub_url].split("-").last.gsub("/","")
+							doc = Nokogiri::HTML(open("http://www.stubhub.com/ticketAPI/restSvc/event/#{@id}"))
+						@dataTable = doc.css('p')
+						Emailer.check_email("#{params[:stubhub_url]}"," Testing #{Rails.env} fetch_stubhub_data").deliver
+						#~ @dataTable = JSON.parse(dataTable.children.to_xml)
+						#~ @dataTable = doc.css('div.dataTables_wrapper')
 		end
 		
 end
