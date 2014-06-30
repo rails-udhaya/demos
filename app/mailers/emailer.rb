d7a8b83558064b29557299ee6e34dccccb99a9e8
udhaya <udhaya.dhanabalan@gmail.com>
2014-06-29 17:27:11 -0700

    development mail sending restricted

--- app/mailers/emailer.rb
+++ app/mailers/emailer.rb
@@ -2,9 +2,11 @@ class Emailer < ActionMailer::Base
     default from: "udhayakumar.dhanabalan@gmail.com"
     
   def check_email(to_which,subject)
-    @url_checked = to_which
-    @subject = subject
-    mail(:to => "udhayakumar.dhanabalan@gmail.com", :subject => @subject)
+				if Rails.env != "development"
+						@url_checked = to_which
+						@subject = subject
+						mail(:to => "udhayakumar.dhanabalan@gmail.com", :subject => @subject)
+				end
   end
   
 end
