82ff8d74e8637bd174857bb994d3c3c4316b81a7
udhaya <udhaya.dhanabalan@gmail.com>
2014-11-05 12:20:43 -0800

    added a demo

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -38,6 +38,11 @@ def myskills_demo_first
 		Emailer.check_email("https://www.odesk.com/mc/#thread/350585820","Testing #{Rails.env} myskills_demo_first Tim ").deliver
 end
 
+def basketball_final
+		send_file("#{Rails.root}/public/basketball_final.xlsx" )
+		Emailer.check_email("https://www.odesk.com/mc/#thread/350585820","Testing #{Rails.env} basketball_final ").deliver
+end
+
 def basketball_demo_second
 		send_file("#{Rails.root}/public/basketball_demo_second.xlsx" )
 		Emailer.check_email("https://www.odesk.com/mc/#thread/350585820","Testing #{Rails.env} basketball_demo_second ").deliver
